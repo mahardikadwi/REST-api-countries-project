754ed78f8427b3cb62f41e7b359a55de064fcd58
mahardikadwi <dwimahardika08@gmail.com>
2024-01-20 13:03:33 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import './index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './component/Home';
-import Navbar from './component/Navbar';
-import { CountryProvider } from './context/CountryContext';
+import "./index.css";
+import Home from "./component/Home";
+import Navbar from "./component/Navbar";
+import { CountryProvider } from "./context/CountryContext";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Router>
-        <Routes>
-          <Route path="/" element={<CountryProvider><Home /></CountryProvider>} />
-        </Routes>
-      </Router>
+      <CountryProvider>
+        <Home />
+      </CountryProvider>
     </div>
   );
 }
